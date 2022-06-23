71b29e3f559898a30ba8b00f3c285d5787f7503d
Krzysztof Szymek <63580276+krzysztofszymek@users.noreply.github.com>
2022-06-23 14:53:36 +0200

    ListItem + Genre style changes

--- src/components/Playlist/playlistComponents/List/ListItem/ListItem.js
+++ src/components/Playlist/playlistComponents/List/ListItem/ListItem.js
@@ -3,11 +3,12 @@ import styles from './ListItem.module.css'
 
 function ListItem(props) {
     const title = props.item.title;
+    const author = props.item.author;
     const genres = props.item.genres;
 
     return (
         <li className={styles.listItem} key={title}>
-            {title}
+            <span className={styles.title}>{author} - {title}</span>
             <div className={styles.genres}>
                 {genres.map((genre) => {
                     return <Genre genre={genre}/>
